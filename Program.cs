aa6e290b106c3cf7a6f726bb8a6876e3d2bb14fa
joaocorner <joaoeduardocorner@gmail.com>
2022-05-19 01:28:40 -0300

    criando arquivos base, encontra-se com erro

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using rpg.src.Entities;
+
+Hero arus = new Hero("Arus", 23, "Knight");
+Wizard wizard = new Wizard("Jenica", 23, "White Wizard");
+
+Console.Write(arus.Attack());
+Console.Write(wizard.Attack());
---
+++ src/Entities/Wizard.cs
@@ -0,0 +1,16 @@
+namespace rpg.src.Entities
+{
+  public class Wizard : Hero
+  {
+    public Wizard(string Name, int Level, string HeroType)
+    {
+      this.Name = Name;
+      this.Level = Level;
+      this.HeroType = HeroType;
+    }
+    public override string Attack()
+    {
+      return this.Name + " atacou com magia";
+    }
+  }
+}
